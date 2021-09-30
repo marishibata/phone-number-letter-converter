2f183b52d635b8cb3ba0804ce18876a11693eeb4
marishibata <76498339+marishibata@users.noreply.github.com>
2021-09-30 20:19:59 +0100

    feat: Keyboard component in TS

--- client/src/components/Keyboard/Keyboard.tsx
+++ client/src/components/Keyboard/Keyboard.tsx
@@ -23,16 +23,16 @@ const Keyboard: FC<{ onClick?: React.MouseEventHandler<HTMLElement> }> = ({ onCl
     <div className="keyboard-container">
       <h2>Keyboard component</h2>
       <div className="keypad-container">
-        <div className="key-number" onClick={onClick}>1</div>
-        <div className="key-number" onClick={onClick}>2</div>
-        <div className="key-number" onClick={onClick}>3</div>
-        <div className="key-number" onClick={onClick}>4</div>
-        <div className="key-number" onClick={onClick}>5</div>
-        <div className="key-number" onClick={onClick}>6</div>
-        <div className="key-number" onClick={onClick}>7</div>
-        <div className="key-number" onClick={onClick}>8</div>
-        <div className="key-number" onClick={onClick}>9</div>
-        <div className="key-number" onClick={onClick}>0</div>
+        <div className="keypad-number" onClick={onClick}>1</div>
+        <div className="keypad-number" onClick={onClick}>2</div>
+        <div className="keypad-number" onClick={onClick}>3</div>
+        <div className="keypad-number" onClick={onClick}>4</div>
+        <div className="keypad-number" onClick={onClick}>5</div>
+        <div className="keypad-number" onClick={onClick}>6</div>
+        <div className="keypad-number" onClick={onClick}>7</div>
+        <div className="keypad-number" onClick={onClick}>8</div>
+        <div className="keypad-number" onClick={onClick}>9</div>
+        <div className="keypad-number" onClick={onClick}>0</div>
       </div>
     </div>
 
