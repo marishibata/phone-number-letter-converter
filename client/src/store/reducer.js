249940569182a6eae7e85952e078b59757ca6af5
marishibata <76498339+marishibata@users.noreply.github.com>
2021-10-01 01:02:03 +0100

    feat: redux reducer

---
+++ client/src/store/reducer.js
@@ -0,0 +1,35 @@
+import { PhoneWordsActionTypes } from "./types";
+
+const initalState = {
+  lettersData: [],
+  isFetching: true,
+  errorMessage: undefined
+}
+
+export const phoneWordsReducer = (state = initalState, action) => {
+  switch (action.type) {
+    case PhoneWordsActionTypes.FETCH_PHONEWORDS_START:
+      return {
+        ...state,
+        isFetching: true
+      }
+    case PhoneWordsActionTypes.FETCH_PHONEWORDS_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        lettersData: action.payload
+      }
+    case PhoneWordsActionTypes.FETCH_PHONEWORDS_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        errorMessage: action.payload
+      }
+    case PhoneWordsActionTypes.CLEAR_PHONEWORDS:
+      return {
+        ...state,
+        lettersData: []
+      }
+    default: return state;
+  }
+}
